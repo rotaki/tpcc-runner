d0374d26ac45ca316fe19ffc299edab8e948c28c
Riki Otaki <rotaki@vulture.a.cs.uchicago.edu>
2023-01-06 11:55:33 -0600

    added comment

--- utils/tsc.hpp
+++ utils/tsc.hpp
@@ -7,5 +7,7 @@ inline uint64_t rdtscp() {
     uint64_t rdx;
     uint32_t aux;
     asm volatile("rdtscp" : "=a"(rax), "=d"(rdx), "=c"(aux)::);
+    // NOTE: rdtscp measures "cycles" not "time"
+    // seconds = cycles / frequency
     return (rdx << 32) | rax;
 }
